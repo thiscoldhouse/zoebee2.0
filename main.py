e2ea68b4936880898b1a9bfc1930f80f78ab5c28
aleruiz <ales.email@gmail.com>
2022-10-07 18:09:46 -0400

    WIP - website working

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import gpt_2_simple as gpt2
 import sys
-from flask import Flask, render_template, send_from_directory, jsonify
+from flask import Flask, render_template, send_from_directory, jsonify, request
 
 app = Flask(__name__)
 
@@ -9,6 +9,25 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/talk', methods=['POST'])
+def talk():
+    data = request.get_json(force=True)
+    if data['length'] > 2000:
+        return jsonify('Length must be less than 1000')
+
+    text = load_and_run_model(
+            data['prefix'],
+            data['temperature'],
+            data['length']
+        )
+    print('=========')
+    print(f'Run with {data}')
+    print(text)
+    print('=========')
+    return jsonify(text)
+
+
 
 @app.route('/static')
 def staticfiles(path):
@@ -29,20 +48,14 @@ def train_model():
     return sess, gpt2
 
 def load_and_run_model(prefix, temperature=.8, length=500):
-    print('*****************************')
-    print('*****************************')
-    print('Prefix: ', prefix)
-    print('Temp: ', temperature)
-    print('Length: ', length)
-    print('*****************************')
-    print('*****************************')
     sess = gpt2.start_tf_sess()
     gpt2.load_gpt2(sess)
-    gpt2.generate(
+    return gpt2.generate(
         sess,
         length=length,
         truncate='<|endoftext|>',
-        prefix=prefix
+        prefix=prefix,
+        return_as_list=True
     )
 
 def train():
@@ -50,35 +63,4 @@ def train():
     gpt2.generate(sess)
 
 if __name__ == '__main__':
-    arg = None
-    try:
-        arg = sys.argv[1]
-    except IndexError:
-        pass
-    if arg is None:
-        app.run(host='0.0.0.0', port=8000, debug=True)
-    elif arg == 'train':
-        train()
-    elif arg == 'run':
-        prefix = None
-        temperature = 1.0
-        length = 500
-        try:
-            prefix = sys.argv[2]
-        except IndexError:
-            pass
-
-        try:
-            temperature = sys.argv[3]
-        except IndexError:
-            pass
-
-        try:
-            length = int(sys.argv[4])
-        except IndexError:
-            pass
-
-        load_and_run_model(prefix, temperature, length)
-
-    else:
-        raise ValueError('Only train or run')
+    app.run(host='0.0.0.0', port=8000, debug=True)
